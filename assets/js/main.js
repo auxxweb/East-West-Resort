8266e802e050f09620bd7ac84699675af78ba928
Rahulraj <rahulrohiniraju@gmail.com>
2025-02-26 14:54:35 +0530

    name changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
- * Template Name: East West Beach Resorts  - v1.3.0
- * Template URL: https://bootstrapmade.com/East West Beach Resorts -bootstrap-construction-website-template/
+ * Template Name: East West Beach Resortss  - v1.3.0
+ * Template URL: https://bootstrapmade.com/East West Beach Resortss -bootstrap-construction-website-template/
  * Author: BootstrapMade.com
  * License: https://bootstrapmade.com/license/
  */
--- assets/scss/Readme.txt
+++ assets/scss/Readme.txt
@@ -1,1 +1,1 @@
-The .scss (Sass) files are only avilable in the pro version. You can buy it from: https://bootstrapmade.com/East West Beach Resorts -bootstrap-construction-website-template/
+The .scss (Sass) files are only avilable in the pro version. You can buy it from: https://bootstrapmade.com/East West Beach Resortss -bootstrap-construction-website-template/
